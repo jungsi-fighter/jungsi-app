4a9a105d5bae9484a7dd2a814aa594e68b0b9882
전열 <jeon-yeol@jeon-yeols-MacBook-Pro.local>
2025-07-13 16:17:43 +0900

    y

--- src/app/you-tube/YouTubeGridClientWrapper.client.tsx
+++ src/app/you-tube/YouTubeGridClientWrapper.client.tsx
@@ -9,7 +9,7 @@ interface YouTubeGridClientProps {
 
 // 실제 YouTubeGridClient 컴포넌트를 동적으로 불러오는 클라이언트 전용 래퍼
 const YouTubeGridClient = dynamic<YouTubeGridClientProps>(
-  () => import('../../components/you-tube/YouTubeGridClient')),
+  () => import('../../components/you-tube/YouTubeGridClient'),
   { ssr: false }
 );
 
